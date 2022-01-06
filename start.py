f18616b1a4354597852649084538cfbdec5d2567
Saif Zidan <saiflion10000@gmail.com>
2022-01-06 18:43:51 +0200

    basic calculator
    printing the sum of two numbers taken from the user

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
-name = input ("Enter your name: ")
-age = input ("Enter your age: ")
-print ("Hello " + name + "! You are " + age)
+num1 = input ("Enter a number: ")
+num2 = input ("Enter another number: ")
+result = num1 + num2
+print (result)
