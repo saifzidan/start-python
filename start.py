ca3ec966be58aa71b1e71adc56f047b997017c77
Saif Zidan <saiflion10000@gmail.com>
2022-01-06 14:02:41 +0200

    dealing with variables
    revise variables

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-print("/__|")
-print("  /_|")
-print(" /__|")
-print("/___|")
+print("There once was a man named George, ")
+print("he was 70 years old. ")
+print("He really liked the name George, ")
+print("but didn't like being 70.")
