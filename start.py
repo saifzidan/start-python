fb976ee3b0cc69a0a58470084385c4b348704015
Saif Zidan <saiflion10000@gmail.com>
2022-01-06 18:55:58 +0200

    madlibs game
    printing a madlibs game using the user input

--- start.py
+++ start.py
@@ -1,4 +1,7 @@
-num1 = input ("Enter a number: ")
-num2 = input ("Enter another number: ")
-result = float (num1) + float (num2)
-print (result)
+color input ("Enter a color: ")
+plural_noun input ("Enter a Plural Noun: ")
+celebrity input ("Enter a celebrity: ")
+print ("Roses are " + color)
+print (+ plural_noun + " are blue")
+print ("I love " + celebrity)
+
