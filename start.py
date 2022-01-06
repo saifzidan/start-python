9c2b3cec0641893fa8c48ee82b2a77156a3f0e7d
Saif Zidan <saiflion10000@gmail.com>
2022-01-06 14:15:57 +0200

    revising variable types
    diffrentiating variable types

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 
 character_name = "Tom"
-character_age = "50"
+character_age = 50
+is_male = False
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age + " years old. ")
 character_name = "Mike"
