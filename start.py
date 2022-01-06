1acc0391506107c8c428eed8b47d43d921d9a746
Saif Zidan <saiflion10000@gmail.com>
2022-01-06 19:05:44 +0200

    lists
    printing a list

--- start.py
+++ start.py
@@ -1,7 +1,2 @@
-color = input ("Enter a color: ")
-plural_noun = input ("Enter a Plural Noun: ")
-celebrity = input ("Enter a celebrity: ")
-print ("Roses are " + color)
-print (plural_noun + " are blue")
-print ("I love " + celebrity)
-
+friends = ["Kevin" , "Karen" , "Jim"]
+print (friends)
