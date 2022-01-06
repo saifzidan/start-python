0aa3189e0e15ae62f6d6d36db8f6972da0418f9e
Saif Zidan <saiflion10000@gmail.com>
2022-01-06 19:22:06 +0200

    lists functions
    declaring a list and inserting it to another

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+lucky_numbers = [4 , 8 , 15 , 16 , 23 , 42]
 friends = ["Kevin" , "Karen" , "Jim" , "Oscar" , "Toby"]
-friends [1] = "Mike"
-print (friends[1])
+friends.extend (lucky_numbers)
+print (friends)
