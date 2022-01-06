b3191606d2a63a4da6a71e0b8ccf1967f8733bd3
Saif Zidan <saiflion10000@gmail.com>
2022-01-06 15:50:50 +0200

    working with strings
    creating a string to work with

--- start.py
+++ start.py
@@ -1,9 +1,1 @@
-
-character_name = "Tom"
-character_age = 50
-is_male = False
-print("There once was a man named " + character_name + ", ")
-print("he was " + character_age + " years old. ")
-character_name = "Mike"
-print("He really liked the name " + character_name + ", ")
-print("but didn't like being " + character_age + ".")
+print ("Giraffe Academy")
