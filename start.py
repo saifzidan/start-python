6cadc785b45e9f6fb73255777ed17d4190cb0ce5
Saif Zidan <saiflion10000@gmail.com>
2022-01-06 14:09:21 +0200

    inserting variables in print statement
    revise variables insert them in print statements

--- start.py
+++ start.py
@@ -1,4 +1,7 @@
-print("There once was a man named George, ")
-print("he was 70 years old. ")
-print("He really liked the name George, ")
-print("but didn't like being 70.")
+
+character_name = "John"
+character_age = "35"
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
