d9a81be0c949ea3f871436f675668b391cc43fc2
Saif Zidan <saiflion10000@gmail.com>
2022-01-09 01:50:10 +0200

    functions
    passing more than a parameter to a function

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-def sayhi (name) :
-    print ("Hello " + name)
-sayhi ("Mike")
-sayhi ("Steve")
+def sayhi (name , age) :
+    print ("Hello " + name + " , you are " + age)
+sayhi ("Mike" , "35")
+sayhi ("Steve" , "70")
