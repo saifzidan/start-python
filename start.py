552d18a88ee57a98c275ea55b8207d9e9800b0dd
Saif Zidan <saiflion10000@gmail.com>
2022-01-09 01:13:30 +0200

    tuples
    playing around with tuples

--- start.py
+++ start.py
@@ -1,4 +1,2 @@
-lucky_numbers = [42 , 4 , 8 , 15 , 16 , 23]
-friends = ["Kevin" , "Karen" , "Jim" , "Jim" , "Oscar" , "Toby"]
-friends2 = friends.copy()
-print (friends2)
+coordinates = (4 , 5)
+print (coordinates[0])
