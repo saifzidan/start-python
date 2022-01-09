3c789580422a13369bc4e79849b5874a11897239
Saif Zidan <saiflion10000@gmail.com>
2022-01-09 05:13:05 +0200

    if
    using if else if and else

--- start.py
+++ start.py
@@ -1,3 +1,10 @@
-def cube (num) :
-    reurn num * num * num
-print (cube (3))
+is_male = True
+is_tall = True
+if is_male and is_tall :
+    print ("You are a tall male")
+elif is_male and not (is_tall) :
+    print ("You are a short male")
+elif not (is_male) and is_tall :
+    print ("You are not a male but are tall")
+else :
+    print ("You are not a male and not tall")
